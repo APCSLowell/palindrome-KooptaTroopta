c661436ec29e71988fed0cfbcd49ebf3d2145852
KooptaTroopta <143123691+KooptaTroopta@users.noreply.github.com>
2024-02-02 11:13:38 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,20 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  String tim = "";
+  for (int i = 0; i<word.length(); i++) {
+    if (Character.isLetter(word.charAt(i)))
+    tim+=word.substring(i,i+1);
+  }
+  if (tim.toLowerCase().equals(reverse(tim).toLowerCase()))
+  return true;
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+    for (int i = str.length()-1; i>=0; i--)
+    sNew += str.substring(i,i+1);
     return sNew;
 }
 }
